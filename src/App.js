5ffe6c22bc12ae554d5bcbf5f684910ca64c399a
toritheterrible <torimpham@gmail.com>
2020-07-29 18:42:45 -0700

    added form and useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [overtime, setOvertime] = useState({
+    hour: 0,
+    minute: 0,
+    otHour: 0,
+    otMinute: 0
+
+  });
+
   return (
+
     <div className="App">
-     
-    </div>
+      <form className="form">
+          <input
+            value={this.state.hour}
+            name="hour"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="hour"
+          />
+          <input
+            value={this.state.minute}
+            name="minute"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="minute"
+          />
+          </form>
+    </div>  
   );
 }
 
